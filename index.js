54aeb57f3152561c1c3d1ff155a340755170c891
connorq <connorquitt@gmail.com>
2023-08-05 13:51:43 -0500

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "Wf"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"}
+  ]
+
+function findWin(dub) {
+    return dub.result === 'W'
+    
+}
+
+function superbowlWin(ar) {
+    if(ar.find(findWin)) {
+        return ar.find(findWin).year
+    } else {
+        return undefined
+    }
+}
+
+console.log(superbowlWin(record))
